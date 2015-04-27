74ab79bd2e4d142d3d73e4f94d94328a971b4c03
Steve <b0rg3r@gmail.com>
2015-04-27 17:25:43 -0500

    SQL statements update

--- app/config/mysql/tables.sql
+++ app/config/mysql/tables.sql
@@ -1,26 +1,48 @@
-
-create table users (
-	id int auto_increment PRIMARY KEY,
-	email varchar(64) NOT NULL,
-	alias varchar(32) NOT NULL,
-	password char(255) NOT NULL
+create table forums (
+	`id` int(11) not null auto_increment,
+	`name` varchar(64) not null,
+	`descr` varchar(255) default null,
+	`permission` int(11) not null default 0,
+	`tsetamp` timestamp not null default current_timestamp,
+	primary key (`id`),
+	unique key `name` (`name`)
 );
 
 create table permissions (
-	id int auto_increment PRIMARY KEY,
-	name varchar(16) NOT NULL
+	`id` int(11) not null auto_increment,
+	`name` varchar(32) not null,
+	primary key (`id`)
 );
-insert into permissions values (null, 'public');
-insert into permissions values (null, 'member');
-insert into permissions values (null, 'mod');
-insert into permissions values (null, 'admin');
 
-create table userpermissions (
-	uid int NOT NULL,
-	pid int NOT NULL,
-	foreign key (uid) references users(id)
-		on delete cascade,
-	foreign key (pid) references permissions(id)
-		on delete cascade
+create table users (
+	`id` int(11) not null auto_increment,
+	`email` varchar(64) not null,
+	`password` varchar(255) not null,
+	`alias` varchar(32) not null,
+	`permission` int(11) not null default 1,
+	primary key (`id`),
+	unique key `email` (`email`)
 );
- 
+
+create table threads (
+	`id` int(11) not null auto_increment,
+	`fid` int(11) not null,
+	`name` varchar(64) not null,
+	`uid` int(11) not null,
+	`tstamp` timestamp not null default current_timestamp,
+	`permission` int(11) not null default 0,
+	primary key (`id`),
+	foreign key (`fid`) references `forums` (`id`) on delete cascade,
+	foreign key (`uid`) references `users` (`id`)
+);
+
+create table messages (
+	`id` int(11) not null auto_increment,
+	`tid` int(11) not null,
+	`content` text,
+	`uid` int(11) not null,
+	`tstamp` timestamp not null default current_timestamp,
+	primary key (`id`),
+	foreign key (`tid`) references `threads` (`id`) on delete cascade,
+	foreign key (`uid`) references `users` (`id`) on delete cascade
+);
