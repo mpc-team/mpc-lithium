dae89d660248850da7f7ce5dc854b7f9761c0c00
Steve <b0rg3r@gmail.com>
2015-09-26 16:13:23 -0500

    Updated Home page look.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -18,14 +18,13 @@ $self = $this
 <div class="panel panel-default">
 	<div class="panel-heading">
 		<center>
-			<h1>mpcgaming.com <br><small>Currently Under Construction<small></h1>
+			<h1>mpcgaming.com <br><small>Currently Under Construction</small></h1>
 		</center>
 	</div>
-	<hr>
-	<section>
-		<h2>News</h2>
-		<div class="well well-sm">
-			<h5>9/26/2015: Passwords can now be reset through the login page.</h5>
-		</div>
-	</section>
 </div>
+<section>
+	<h2>News</h2>
+	<div class="well well-sm">
+		<h5>9/26/2015: Passwords can now be reset through the login page.</h5>
+	</div>
+</section>
