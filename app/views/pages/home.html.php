354fb4be50f3d94b12e3b9de724b074bc56fcff0
Steve <b0rg3r@gmail.com>
2015-09-26 15:53:07 -0500

    Added a preliminary 'News' section to Home page.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -19,5 +19,13 @@ $self = $this
 	<div class="panel-heading">
 		<center>
 			<h1>mpcgaming.com <br><small>Currently Under Construction<small></h1>
+		</center>
 	</div>
+	<hr>
+	<section>
+		<h2>News</h2>
+		<div class="well well-sm">
+			<h5>9/26/2015: Passwords can now be reset through the login page.</h5>
+		</div>
+	</section>
 </div>
