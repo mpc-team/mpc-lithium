a82f9e28ab997d3a4dadc8a497b952589b961ccd
Steve <b0rg3r@gmail.com>
2015-12-06 17:49:27 -0600

    Added columns for Announcements/News.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -15,23 +15,25 @@ $this->title('Home');
 $self = $this
 
 ?>
-<h2>Announcements</h2>
-		<div class="well well-sm">
-			<h5>5 in a row</h5>
-			<img src="http://www.mpcgaming.com/app/webroot/img/clashofclans/assassins/victory10302015.png" class="img-rounded img-responsive" alt="victory103002015.png" style="margin: auto;" />
-		</div>
-		<div class="well well-sm">
-			<h5>Congrats MPC Clash of Clans: <br />
-				MPC Assassins wins 4th war in a row, and gets promoted to Clan Level 2!</h5>
-			<img src="http://www.mpcgaming.com/app/webroot/img/clashofclans/assassins/clanlevel2.png" class="img-rounded img-responsive" alt="clanlevel2.png" style="margin: auto;" />
-		</div>
-		<div class="well well-sm">
-			<h5>10/25/15: 1PM EST, Noon CST Starcraft 2 Heart of the Swarm Tournament on sunday 1-5PM EST for 2 vs 2</h5>
-		</div>
-		<div class="well well-sm">
-			<h5>10/20/2015: Clash of Clans MPC assassins Starting War against Furia De Roig</h5>
-		</div>
-<section>
+<section id="announcements" class="col-md-8">
+    <h2>Announcements</h2>
+    <div class="well well-sm">
+	    <h5>5 in a row</h5>
+	    <img src="http://www.mpcgaming.com/app/webroot/img/clashofclans/assassins/victory10302015.png" class="img-rounded img-responsive" alt="victory103002015.png" style="margin: auto;" />
+    </div>
+    <div class="well well-sm">
+	    <h5>Congrats MPC Clash of Clans: <br />
+		    MPC Assassins wins 4th war in a row, and gets promoted to Clan Level 2!</h5>
+	    <img src="http://www.mpcgaming.com/app/webroot/img/clashofclans/assassins/clanlevel2.png" class="img-rounded img-responsive" alt="clanlevel2.png" style="margin: auto;" />
+    </div>
+    <div class="well well-sm">
+	    <h5>10/25/15: 1PM EST, Noon CST Starcraft 2 Heart of the Swarm Tournament on sunday 1-5PM EST for 2 vs 2</h5>
+    </div>
+    <div class="well well-sm">
+	    <h5>10/20/2015: Clash of Clans MPC assassins Starting War against Furia De Roig</h5>
+    </div>
+</section>
+<section id="news" class="col-md-4">
 	<h2>News</h2>
 	<div class="well well-sm">
 			<h5>11/06/2015: Avatars are now in complete working order!</h5>
