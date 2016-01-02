42d44698418df0903c1275a939b80b3f8896271a
AcidSnake <keatingc88@gmail.com>
2016-01-02 03:45:52 -0600

    Deleted Ventrilo information + content, started information section for discord.

---
+++ app/views/elements/information/communication.html.php
@@ -0,0 +1,1 @@
+<small>Communication</small>
--- app/views/information/index.html.php
+++ app/views/information/index.html.php
@@ -4,31 +4,89 @@ $this->title('Information');
 
 $self = $this;
 
+//the numeric value inbetween $info and Indexes is the corresponding Row. Each row has 3 .Well Class Divs.
+$info1Indexes = array
+    (
+        'aboutus',
+        'communication',
+        'members',
+    );
+
 ?>
 <div id="mpc-information">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="well">
-                <small>Test</small>
+    <div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="false">
+        <div class="row">
+
+            <?php foreach($info1Indexes as $infoIndex): ?>
+
+            <div class="col-md-4">
+                <div class="well" role="button" data-toggle="collapse" data-parent="#accordion2" href="#info-<?= $infoIndex ?>-collapse" aria-expanded="false" aria-controls="info-<?= $infoIndex ?>-collapse">
+                    <img src="/img/information/<?= $infoIndex ?>-banner.png" alt="<?= $infoIndex ?>-banner.png" class="img-responsive img-rounded" id="info-<?= $infoIndex ?>-banner" />
+                </div>
+            </div>
+
+            <?php endforeach; ?>
+
+        </div>
+        <div class="row">
+
+            <?php foreach($info1Indexes as $infoIndex): ?>
+
+            <div id="info-<?= $infoIndex ?>-collapse" class="panel-collapse collapse" role="tabpanel" aria-labelledby="">
+                <div class="well">
+                    <?= $this->view()->render(
+                        array('element' => 'information/' . $infoIndex)
+                    ); ?>
+                </div>
+            </div>
+
+            <?php endforeach; ?>
+
+        </div>
+
+    </div>
+    <div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingOne">
+                <h4 class="panel-title">
+                    <a role="button" data-toggle="collapse" data-parent="#accordion1" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Collapsible Group Item #1
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="well">
-                <small>Test</small>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingTwo">
+                <h4 class="panel-title">
+                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion1" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Collapsible Group Item #2
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="panel-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="well">
-                <small>Test</small>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion1" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Collapsible Group Item #3
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                </div>
             </div>
         </div>
     </div>
 </div>
-<script>
-
-    $(document).ready(function()
-    {
-               
-    });//End Document Ready Function
-
-</script>
