8ad830b512254cbb0507475c3f798b20d2b0e7cf
AcidSnake <keatingc88@gmail.com>
2016-01-18 19:12:09 -0600

    updated the index to /connect folder.

--- app/views/connect/index.html.php
+++ app/views/connect/index.html.php
@@ -7,15 +7,17 @@ $self = $this;
 ?>
 <div id="connectdiscord">
     <div class="row">
-        <img src="/img/connect/discord-banner.png" alt="discord-banner.png" class="img-rounded img-responsive" id="connect-discordbanner">
+        <img src="/img/connect/discord-banner.png" alt="discord-banner.png" class="img-rounded img-responsive" id="discordbanner">
     </div>
-    <div class="btn-group pull-right" role="group" aria-label="discord-btns" id="discord-btns">
-      <a role="button" class="btn btn-edit" href="https://discordapp.com/apps" target="_blank">Download</a>
-      <a role="button" class="btn btn-edit" data-toggle="collapse" href="#serverstatus-collapse" aria-expanded="false" aria-controls="serverstatus-collapse">Server Status</a>
+    <div class="btn-group pull-right" role="group" aria-label="discord-btns" id="discord-btns">      
+      <a role="button" class="btn btn-edit" data-toggle="collapse" href="#serverstatus-collapse" aria-expanded="false" aria-controls="serverstatus-collapse">Show Server</a>
       <a role="button" class="btn btn-edit" href="https://discord.gg/0iDKElOIs9m3KN86" target="_blank">Server Connect</a>
+      <a role="button" class="btn btn-edit" href="https://discordapp.com/apps" target="_blank">Download</a>
+      <a role="button" class="btn btn-edit" href="https://www.youtube.com/channel/UCZ5XnGb-3t7jCkXdawN2tkA/videos" target="_blank">YouTube</a>
     </div>        
-    <div
     <div class="collapse" id="serverstatus-collapse">
         <iframe src="https://discordapp.com/widget?id=127671174648823808&theme=dark" allowtransparency="true"></iframe>
+        <br />
+        <a role="button" class="btn btn-edit" data-toggle="collapse" href="#serverstatus-collapse" aria-expanded="false" aria-controls="serverstatus-collapse" id="closeserver-btn">Close</a>
     </div>
 </div>  
--- app/views/starcraft2/index.html.php
+++ app/views/starcraft2/index.html.php
@@ -10,18 +10,11 @@ $self = $this;
 <?php foreach($dir as $file): ?>
 	<p><?= $file; ?></p>
 <?php endforeach; ?>
-<div id="starcraft-2-lotv">
+<div id="starcraft2lotv">
     <div class="row">
-        <img src="/img/sc2/head-banner.png" alt="head-banner.png" class="img-responsive img-rounded" id="sc2lotv-headbanner" />
-        <img src="/img/mpc/mpcgaming-logo.png" alt="mpcgaming-logo.png" class="img-responsive img-rounded" id="sc2-mpc-logo"/>
+        <img src="/img/sc2/sc2lotvbanner.png" alt="sc2lotvbanner.png" class="img-responsive img-rounded" id="sc2lotvbanner" />
     </div>
     <div class="row">
-        <div class="panel">
-            <div class="panel-body">
-                <div class="embed-responsive embed-responsive-16by9">
-                  <iframe class="embed-responsive-item" src="..."></iframe>
-                </div>
-            </div>
-        </div>
+        
     </div>
 </div>
