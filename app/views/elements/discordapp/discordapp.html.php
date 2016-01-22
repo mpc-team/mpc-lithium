e624447653191f28ab358ab8aa5048cd2ee6c35d
Steve <b0rg3r@gmail.com>
2016-01-21 23:38:33 -0600

    Some Discord shit that wasn't me.

--- app/views/elements/discordapp/discordapp.html.php
+++ app/views/elements/discordapp/discordapp.html.php
@@ -1,3 +1,3 @@
 <div id="discord-app">
-        <iframe src="https://discordapp.com/widget?id=127671174648823808&theme=dark" allowtransparency="true"></iframe>
+    <iframe src="https://discordapp.com/widget?id=127671174648823808&theme=dark" allowtransparency="true"></iframe>
 </div>
