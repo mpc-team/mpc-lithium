e4ee5eb585b373ccf3a3527c692b9ebd22fc3ed4
AcidSnake <keatingc88@gmail.com>
2016-01-29 02:30:34 -0600

    polished sc2 , add image / endday

--- app/views/starcraft2/index.html.php
+++ app/views/starcraft2/index.html.php
@@ -50,14 +50,12 @@ $self = $this;
             <div class="panel-group">
                 <div class="panel">
                     <div class="panel-heading">
-                        <h3 class="panel-title">
-                            The Trump's Revenge
-                        </h3>
-                        <small>Genre: Survival</small>
+                        <img src="/img/sc2/trumpsrevenge-banner.png" alt="trumpsrevenger-banner.png" class="img-rounded img-responsive" id="trumpsrevenger-bannerimg" />
                     </div>
                     <div class="panel-body" style="margin: 15px;">
                         <button type="button" data-toggle="collapse" data-target="#trumpshow" aria-expanded="false" aria-controls="trumpshow" style="color: #000; margin-bottom: 5px;">View Description</button>
                         <div class="collapse" id="trumpshow">
+                            <small>Genre: Survival</small>
                             <p class="text-indent">The Year is 2026, 10 years after Trump's defeat at the 2016 elections. For the last ten years, the name trump has fallen into a pit of obscurity as he fell away from the public eye, consumed by grief. In 2022 he disappears completely. No one can find him, or his family, the trumpz tower is deserted and the brand forgotten. Little happens in those years following Trump's disappearance. In 2021, Isis was defeated, and the USA felt its problems were over. But following Trump's disappearance, factories received millions of dollars from unknown sources in exchange for their resources. Little was said about this disturbance, money to the factories was money no matter who spent it. No one suspected trump, he was long forgotten, no one could even recall him, no one that is, but trump! Trump never forgot, he lived in his cave plotting, and his army grew with each import of resources, his forces grew and evolved as his scientists experimented on them in exchange for tens of millions in cash. Through biological engineering and mass production, his army began to assemble. He was ready to begin the true age of trump. The year is now 2026, trump rose from his underground dwelling in a wave of splendor, his armies decimating all that stood in his way. The USA army is ruins, their allies doing little to prevent trump from conquering everything he had longed for, all that stands between him and the white house is you, the elite bodyguard of the president. The universe hangs in the balance, do not let trump reach the Whitehouse</p>
                         </div>
                     </div>
@@ -129,7 +127,6 @@ $self = $this;
                             <div class="row">
                                 <p>Frequently asked questions, and basic understanding on the process of the clan wars.</p>
                                 <br />
-                                <br />
                             </div>
                         </div>                
                     </div>
@@ -145,7 +142,7 @@ $self = $this;
         </h3>
         <div class="row">
             <div class="col-md-12">
-                <a role="button" href="#sc2caster-vaevictissc-collapse" data-toggle="collapse" aria-expnded="false" aria-controls="sc2caster-vaevictissc-collapse">
+                <a class="preventdefault">
                     <div class="panel-group">
                             <div class="panel">
                             <div class="panel-heading">
@@ -155,10 +152,20 @@ $self = $this;
                             </div>
                             <div class="panel-body">
                                 <div class="row">
-                                    <p>Check the Sign Up's page to see the next cast, and Starcraft 2 Events.</p>
-                                    <br />
-                                    <br />
-                                    <span class="glyphicon glyphicon-arrow-down" style="margin-left: 50%;"></span>
+                                     <div class="row" style="padding-top: 5%; padding-left: 8%;">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <iframe class="embed-responsive-item" src="http://www.twitch.tv/widgets/live_embed_player.swf?channel=vaevictissc"></iframe>
+                                        </div>                
+                                        <a role="button" class="btn btn-lg" href="#casterchat-vaevictissc-collapse" data-toggle="collapse" aria-expnded="false" aria-controls="casterchat-vaevictissc-collapse" id="castershowchat-vaevictissc-collapsebtn">Show Chat</a>
+                                    </div>
+                                    <div class="collapse text-center" id="casterchat-vaevictissc-collapse" style="padding-top: 5%; padding-left: 8%;">
+                                        <div class="embed-responsive embed-responsive-4by3">
+                                            <iframe frameborder="0" scrolling="yes" src="http://twitch.tv/vaevictissc/chat?popout=">
+                                            </iframe>
+                                        </div>
+                                        <p>Not Showing up properly? View His <a class="btn" href="http://www.twitch.tv/vaevictissc" target="_blank">Page</a>.</p>
+                                    </div>    
+                                    <!--<span class="glyphicon glyphicon-arrow-down" style="margin-left: 50%;"></span>-->
                                 </div>
                             </div>                
                         </div>
@@ -167,6 +174,12 @@ $self = $this;
             </a>
         </div>
     </div>
+    <script>
+        $('#starcraft2lotv .preventdefault .panel').mouseover(function() {
+           event.preventdefault(); 
+           $(this).css('background-color','#fff');
+        });
+    </script>
     <!--hidden objects-->
         <div class="modal fade" id="clanwarsmodal" tabindex="-1" role="dialog" aria-labelledby="clanwarsmodal-label">
           <div class="modal-dialog" role="document">
@@ -179,56 +192,45 @@ $self = $this;
                 <div class="row">
                     <div class="media">
                         <div class="media-left">
-                        <a href="#">
-                            <img class="media-object sc2modalbullet" src="/img/sc2/modalbullet.png" alt="modalbullet.png" />
-                        </a>
+                            <a href="#">
+                                <img class="media-object sc2modalbullet" src="/img/sc2/modalbullet.png" alt="modalbullet.png" />
+                            </a>
                         </div>
                         <div class="media-body">
                         <h4 class="media-heading">Description</h4>
                             <small>Clan Wars is a competition of 1 vs 1, but as a clan vs clan tournament. The victor will have the most points when all of the members from the opposing clan has been defeated. Normally, the clans will have will have a best out of a odd number for the clan vs clan, but also per player vs player match ups.</small>
                         </div>
                     </div>
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object sc2modalbullet" src="/img/sc2/modalbullet.png" alt="modalbullet.png" />
-                        </a>
-                        </div>
-                        <div class="media-body">
-                        <h4 class="media-heading">Map Choice and Decision</h4>
-                            <small>Maps are based on the current season's 1 vs 1 league maps. Each clan will veto a map until a single map stands remaining. After the two players from both clans have finished their first game, the losing player will decide the next map. Once a map has been played, that map cannot be chosen until all the rest have been placed atleast once, then a new cycle begins.</small>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object sc2modalbullet" src="/img/sc2/modalbullet.png" alt="modalbullet.png" />
+                            </a>
+                            </div>
+                            <div class="media-body">
+                            <h4 class="media-heading">Map Choice and Decision</h4>
+                                <small>Maps are based on the current season's 1 vs 1 league maps. Each clan will veto a map until a single map stands remaining. After the two players from both clans have finished their first game, the losing player will decide the next map. Once a map has been played, that map cannot be chosen until all the rest have been placed atleast once, then a new cycle begins.</small>
+                            </div>
                         </div>
                     </div>
-                    <div class="media-left">
-                        <a href="#">
-                            <img class="media-object sc2modalbullet" src="/img/sc2/modalbullet.png" alt="modalbullet.png" />
-                        </a>
-                        </div>
-                        <div class="media-body">
-                        <h4 class="media-heading">Casting Rules</h4>
-                            <small>Caster's stream broadcast will have a requirement of 3 minute real time delay. Only Casters will be allowed in the game, unless accompanied by another player, but sometimes if agreement is made between both clans, then additional players may observe.</small>
+                    <div class="media">
+                        <div class="media-left">
+                            <a href="#">
+                                <img class="media-object sc2modalbullet" src="/img/sc2/modalbullet.png" alt="modalbullet.png" />
+                            </a>
+                            </div>
+                            <div class="media-body">
+                            <h4 class="media-heading">Casting Rules</h4>
+                                <small>Caster's stream broadcast will have a requirement of 3 minute real time delay. Only Casters will be allowed in the game, unless accompanied by another player, but sometimes if agreement is made between both clans, then additional players may observe.</small>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <a type="button" class="btn btn-edit" data-dismiss="modal">Close</a>
                     <a type="button" class="btn btn-edit" href="http://www.mpcgaming.com/board/view/11">Register</a>
+                    <a type="button" class="btn btn-edit" data-dismiss="modal">Close</a>                    
                 </div>
             </div>
         </div>
     </div>      
-    <div class="collapse" id="sc2caster-vaevictissc-collapse">
-        <div class="row" style="padding-top: 5%; padding-left: 8%;">
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="http://www.twitch.tv/widgets/live_embed_player.swf?channel=vaevictissc"></iframe>
-            </div>                
-            <a role="button" class="btn btn-lg" href="#casterchat-vaevictissc-collapse" data-toggle="collapse" aria-expnded="false" aria-controls="casterchat-vaevictissc-collapse" style="background-color: #fff; border: 1px #00fff; color: #000;">Show Chat</a>
-        </div>
-        <div class="collapse text-center" id="casterchat-vaevictissc-collapse" style="padding-top: 5%; padding-left: 8%;">
-            <div class="embed-responsive embed-responsive-4by3">
-                <iframe frameborder="0" scrolling="yes" src="http://twitch.tv/vaevictissc/chat?popout=">
-                </iframe>
-            </div>
-            <p>Now Showing up properly? View His <a class="btn" href="http://www.twitch.tv/vaevictissc" target="_blank">Page</a>.</p>
-        </div>          
-    </div> 
 </div>
