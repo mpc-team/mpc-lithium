783f6aad959ed112543e1b1b4042a810386ae0a4
AcidSnake <keatingc88@gmail.com>
2016-02-15 00:38:07 -0600

    Updated the view/heroes_of_the_storm/index.html.php and webroot.css

--- app/views/heroes_of_the_storm/index.html.php
+++ app/views/heroes_of_the_storm/index.html.php
@@ -46,7 +46,7 @@ $self = $this;
                             
                             http://www.hotslogs.com/PlayerSearch?Name=acidsnake
                 
-                        --->
+                        -->
                    </div><!--col-md-12-->
                 </div><!--ROW-->
             </div><!--body-->
@@ -56,22 +56,197 @@ $self = $this;
         </div>
     </div>
     <h3>
-        Chracter
+        Character
         <small>
             <?= $this->title ?>
         </small>
     </h3>
-    <div class="row">
+    <div class="row" id="hots-characters">
         <div class="panel">
             <div class="panel-heading">
                 <small>Knowing your Character Well</small>
             </div>
             <div class="panel-body">
-                
-            </div>
+                <?php 
+
+                $classes = array(
+                        'warriors'=>
+                        'Warriors',
+                        'assassins'=>
+                        'Assassins',
+                        'supports'=>
+                        'Supports',
+                        'specialists'=>
+                        'Specialists',
+                    );
+
+
+                   $warriors = array(
+                        'anub-arak' => 'Anub\'arak',
+                        'artanis' => 'Artanis',
+                        'arthas' => 'Arthas',
+                        'chen' => 'Chen',
+                        'cho-gall' => 'Cho-Gall',
+                        'diablo' => 'Diablo',
+                        'etc' => 'E.T.C.',
+                        'johanna' => 'Johanna',
+                        'leoric' => 'Leoric',
+                        'muradin' => 'Muradin',
+                        'rexxar' => 'Rexxar',
+                        'sonya' => 'Sonya',
+                        'stitches' => 'Stitches',
+                        'tyrael' => 'Tyrael',               
+                    );
+
+                   $assassins = array(
+
+                        'falstad' => 'Falstad',
+                        'greymane' => 'GreyMane',
+                        'illidan' => 'Illidan',
+                        'jaina' => 'Jaina',
+                        'kael-thas' => 'Kael-Thas',
+                        'kerrigan' =>'Kerrigan',
+                        'lunara' =>'Lunara',
+                        'nova' =>'Nova',
+                        'raynor'=>'Raynor',
+                        'the-butcher'=>'The Butcher',
+                        'thrall'=>'Thrall',
+                        'tychus'=>'Tychus',
+                        'valla' =>'Valla',
+                        'zeratul' =>'Zeratul',                
+
+                    ); 
+
+                    $supports = array(
+                        
+                        'brightwing' => 'BrightWing',
+                        'kharazim'=>'Kharazim',
+                        'li-li'=>'Li Li',
+                        'lt-morales'=>'Lt. Morales',
+                        'malfurion'=>'Malfurion',
+                        'rehgar'=>'Rehgar',
+                        'tassadar'=>'Tassadar',
+                        'tyrande'=>'Tyrande',
+                        'uther'=>'Uther',
+                    );
+
+                    $specialists = array(
+
+                        'abathur' =>'Abathur',
+                        'azmodan'=>'Azmodan',
+                        'gazlowe'=>'Gazlowe',
+                        'murky'=>'Murky',
+                        'nazeebo'=>'Nazeebo',
+                        'sgt-hammer'=>'Sgt. Hammer',
+                        'sylvanas'=>'Sylvanas',
+                        'the-lost-vikings'=>'The Lost Viking',
+                        'zagara'=>'Zagara',
+                        
+                    );
+
+                ?>
+
+
+                <!--control buttons for classes-->
+                <div class="row" style="margin-bottom: 5px;">
+                    <div class="btn-group btn-group-justified" role="group">
+                        <?php foreach($classes as $class => $classDisplay): ?>
+                            <a class="btn btn-edit" role="button" data-toggle="collapse" href="#collapse<?= $class ?>" aria-expanded="false" aria-controls="collapse<?= $class ?>" data-parent="#accordion"><?= $class ?></a>
+                        <?php endforeach; ?>
+                    </div><!--btn group-->
+                </div><!-- row-->
+
+
+
+
+                <!-- information on the classes -->
+                <div class="row">
+                    <?php foreach($classes as $class => $classDisplay): ?>
+                    <div class="collapse" id="collapse<?= $class ?>">
+                        <?php if($class == 'warriors'): ?>
+                        <div class="row">
+                            <?php foreach($warriors as $id => $displayName): ?>                                         
+                                <div class="col-xs-4 class-portrait" style="padding: 2px 1px 2px 1px;">
+                                    <a href="#">
+                                        <div class="panel">
+                                            <div class="panel-heading">
+                                                <img src="/img/hots/heroes/<?= $id ?>/portrait.png" alt="<?= $id ?>.png" class="img-rounded img-responsive " />
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                <h2 class="panel-title">
+                                                    <?= $displayName ?>
+                                                </h2>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>                                
+                            <?php endforeach; ?>
+                        </div>
+                        <?php elseif($class == 'assassins'): ?>
+                        <div class="row">
+                            <?php foreach($assassins as $id => $displayName): ?>
+                                <div class="col-xs-4 class-portrait" style="padding: 2px 1px 2px 1px;">
+                                    <a href="#">
+                                        <div class="panel">
+                                            <div class="panel-heading">
+                                                <img src="/img/hots/heroes/<?= $id ?>/portrait.png" alt="<?= $id ?>.png" class="img-rounded img-responsive " />
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                <h2 class="panel-title">
+                                                    <?= $displayName ?>
+                                                </h2>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>           
+                            <?php endforeach; ?>
+                        </div>
+                        <?php elseif($class == 'supports'): ?>
+                        <div class="row">
+                            <?php foreach($supports as $id => $displayName): ?>
+                                <div class="col-xs-4 class-portrait" style="padding: 2px 1px 2px 1px;">
+                                    <a href="#">
+                                        <div class="panel">
+                                            <div class="panel-heading">
+                                                <img src="/img/hots/heroes/<?= $id ?>/portrait.png" alt="<?= $id ?>.png" class="img-rounded img-responsive " />
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                <h2 class="panel-title">
+                                                    <?= $displayName ?>
+                                                </h2>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>           
+                            <?php endforeach; ?>
+                        </div>
+                        <?php elseif($class == 'specialists'): ?>
+                        <div class="row">
+                            <?php foreach($specialists as $id => $displayName): ?>
+                                 <div class="col-xs-4 class-portrait" style="padding: 2px 1px 2px 1px;">
+                                    <a href="#">
+                                        <div class="panel">
+                                            <div class="panel-heading">
+                                                <img src="/img/hots/heroes/<?= $id ?>/portrait.png" alt="<?= $id ?>.png" class="img-rounded img-responsive " />
+                                            </div>
+                                            <div class="panel-body text-center">
+                                                <h2 class="panel-title">
+                                                    <?= $displayName ?>
+                                                </h2>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>           
+                            <?php endforeach; ?>
+                        </div>
+                        <?php endif; ?>
+                    </div><!--collapse-->
+                <?php endforeach; ?>
+                </div><!--row in parent panel--> 
+            </div><!--panelbody-->
             <div class="panel-footer">
                 <small>Review</small>
-            </div>
+            </div><!--panel-footer-->
         </div>
     </div>
 </div>
