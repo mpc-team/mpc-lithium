0613af59c6a1718ad94afea150a1c835a1ac6d38
Steve <b0rg3r@gmail.com>
2016-03-25 18:53:52 -0500

    Reduced Events that are displayed in Upcoming.

--- app/webroot/js/utils/events/EventsUpcoming.js
+++ app/webroot/js/utils/events/EventsUpcoming.js
@@ -6,7 +6,7 @@ var EventsUpcoming = {};
 ------------------------------------------------------------------------------------------------ */
 
 /* Request Upcoming Events from the Server */
-EventsUpcoming.RequestEventsLimit = '10';
+EventsUpcoming.RequestEventsLimit = 5;
 EventsUpcoming.RequestEventsUri = '/api/events/upcoming?limit=' + EventsUpcoming.RequestEventsLimit;
 EventsUpcoming.RequestEvents = function (callback) { $.get(EventsUpcoming.RequestEventsUri, null, callback); }
 
@@ -31,19 +31,23 @@ EventsUpcoming.UI.Event = function (eventObject)
 	var dateFormat = 'dddd, MMM. Do YYYY';
 	var description = (eventObject.description != null) ? eventObject.description : "";
 	this.eventObject = eventObject;
-
-
-
 	this.html =
 	"<div class='row'>" +
 		"<div class='panel-group' style='margin-bottom: 7px'>" +
 			"<div class='panel panel-default bordered-panel shadow-med-1'>" +
 				"<div class='panel-heading'>" +
-					"<h3>" + eventObject.title + "</h3>" +
+					"<center>" +
+						"<h3>" + eventObject.title + "</h3>" +
+					"</center>" +
 				"</div>" +
-				"<p style='padding:10px; font-size: 9pt; margin:0;'>" + description + "</p>" +
+				"<center>" +
+					"<p style='padding:10px; font-size: 9pt; margin:0;'>" +
+						description +
+					"</p>" +
+				"</center>" +
 				"<div class='panel-footer'>" +
 					"<center>" +
+						"<br />" +
 						"<p style='font-size: 10pt; margin-bottom:0; padding: 3px 5px;'>" +
 							startDate.format(dateFormat) + " at " + startDate.format(startFormat) + "<br />" +
 							"to" + "<br />" +
@@ -52,6 +56,7 @@ EventsUpcoming.UI.Event = function (eventObject)
 						"<p style='font-size: 9pt; margin-bottom: 0;'>" +
 							"(" + duration + ")" +
 						"</p>" +
+						"<br />" +
 					"</center>" +
 				"</div>" +
 			"</div>" +
